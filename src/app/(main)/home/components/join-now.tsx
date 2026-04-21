f7fe605d3b9cf9c7cec61c733e1da32c9f805265
khiemlevar <an38570@gmail.com>
2026-04-21 13:58:56 +0700

    feat: add JoinNow component with ripple background effect and device detection hook

--- src/app/(main)/home/components/join-now.tsx
+++ src/app/(main)/home/components/join-now.tsx
@@ -1,10 +1,17 @@
+'use client';
 import Image from 'next/image';
 
 import { cn } from '@/lib/utils';
+import RippleBackground from '@/components/ui/ripple-background';
 
 const JoinNow = () => {
   return (
-    <div className="relative flex h-screen max-h-[900px] w-full items-center justify-center bg-[url('/images/lp/bg1.png')] bg-center bg-no-repeat xl:max-h-screen xl:bg-cover">
+    <RippleBackground
+      image={'/images/lp/bg1.webp'}
+      className='relative flex h-dvh md:h-[calc(100dvh-30px)] w-full items-center justify-center xl:max-h-screen'
+      intensity={5}
+      rippleSize={40}
+    >
       <div className='container flex flex-col items-center'>
         <Image src={'/images/logo-header.png'} width={288.48} height={192.27} alt='logo' className='h-[184px] w-auto' />
         <p className='mt-6 mb-3 text-center font-extrabold text-2xl text-yellow-300 uppercase tracking-widest drop-shadow-[0_2px_0_#000] [text-shadow:0_0_10px_rgba(255,200,0,0.8),0_0_20px_rgba(255,150,0,0.6)] md:text-3xl '>
@@ -30,7 +37,7 @@ const JoinNow = () => {
           <span className='absolute inset-0 rounded-xl bg-yellow-300 opacity-20 blur-md'></span>
         </button>
       </div>
-    </div>
+    </RippleBackground>
   );
 };
 
